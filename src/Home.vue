c316bdf178050800c6b7ecc947f80e2748c4138d
Favour <toluwaniokunowo@gmail.com>
2022-03-08 13:50:49 +0100

    add mobile view

---
+++ src/Home.vue
@@ -0,0 +1,173 @@
+<template>
+    <main class="w-full min-h-screen text-white bg-black">
+        <div class="mx-auto max-w-7xl">
+
+            <header>
+                <img alt="Logo" class="mx-auto" src="./assets/logo.png" />
+                <img alt="Background Image" class="md:hidden" src="./assets/Mobile%20Portrait.jpg">
+            </header>
+
+            <section class="px-6 py-4">
+                <div class="">
+                    <h1 class="text-xl font-bold text-center">ABOUT</h1>
+                    <hr class="w-20 h-1.5 bg-amber-500 mx-auto">
+
+                    <p class="mt-4 text-lg text-justify">
+                        A FORMER SOLDIER OF THE FALLEN ANCIENT EMPIRE OF OYO SEARCHES FOR REASON WHEN FATE AND HIS PAST
+                        COME
+                        HUNTING. REMILEKUN IS THRUST BACK INTO HIS ARMOUR WHEN HIS QUIET LIFE IN RETIREMENT IS
+                        THREATENED BY
+                        A MYSTERIOUS FORCE KILLING AND BURNING TO GET HIM. HE MUST CONFRONT DESTINY, MUST MAKE
+                        COMPANIONS OF
+                        ENTITIES ANCIENT AND POWERFUL AND TO FIND TRUTH AND REDEMPTION FOR THE FATE OF MEN AND GODS MAY
+                        REST
+                        OF THE STRENGTH OF HIS HEART AND THE SHARPNESS OF HIS BLADE.
+                    </p>
+                </div>
+            </section>
+
+            <!-- Characters -->
+            <section class="px-6 py-4">
+                <h1 class="text-xl font-bold text-center">CHARACTERS</h1>
+                <hr class="w-36 h-1.5 bg-amber-500 mx-auto">
+
+                <div class="grid grid-cols-1 mt-4 md:grid-cols-2 lg:grid-cols-4">
+                    <div>
+                        <img alt="" class="rounded-t-lg" src="./assets/cg_01.png">
+
+                        <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
+                            <h3 class="text-2xl font-bold uppercase">Bamiloye</h3>
+                            <p class="border-b border-gray-400"></p>
+                            <p class="text-lg text-justify uppercase">
+                                YEMISI'S ELDER BROTHER AND FIRST CHILD OF THE FAMILY. BORN AWAY FROM THE VILLAGE AND
+                                CATERED FOR BY HIS GRANDPARENTS, BAMILOYE JOINS HIS FAMILY AFTER THE DEATH OF HIS
+                                FATHER.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img alt="" class="rounded-t-lg" src="./assets/cg_02.png">
+
+                        <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
+                            <h3 class="text-2xl font-bold uppercase">Iya Yemisi</h3>
+                            <p class="border-b border-gray-400"></p>
+                            <p class="text-lg text-justify uppercase">
+                                'IF THIS IS WHAT BEING ALONE FEELS LIKE, I WOULDN'T MIND BEING ALONE FOREVER'. SHE IS
+                                THE WIDOWED MOTHER OF BAMILOYE AND YEMISI. SHE IS A HARD-WORKING FARMER AND GOAT-HERDER.
+                                SHE, LIKE ALL MOTHERS, DESIRES THE BEST FOR HER CHILDREN.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img alt="" class="rounded-t-lg" src="./assets/cg_03.png">
+
+                        <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
+                            <h3 class="text-2xl font-bold uppercase">Yemisi</h3>
+                            <p class="border-b border-gray-400"></p>
+                            <p class="text-lg text-justify uppercase">
+                                'BAMI DOESN'T NOTICE ANYTHING'. A CHILD OF SIX. SMART AND INTUITIVE AND A BIT OF A
+                                TALKATIVE. SHE IS HEAVILY AFFECTIONATE AND A HAPPY CHILD AND THE ONLY PERSON WE SEE IN
+                                THE VILLAGE THAT IS SPECIAL ENOUGH TO GAIN REMI'S, WHOM SHE FONDLY CALLS REMI,
+                                AFFECTION. HE ALSO FONDLY CALLS HER SIMI. SHE ISN'T A NORMAL GIRL CHILD AS SHE UNDERGOES
+                                A MILD WARRIOR TRAINING UNDER REMI, WHO TEACHES HER HUNTING AND MARKSMANSHIP.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img alt="" class="rounded-t-lg" src="./assets/cg_04.png">
+
+                        <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
+                            <h3 class="text-2xl font-bold uppercase">Remilekun</h3>
+                            <p class="border-b border-gray-400"></p>
+                            <p class="text-lg text-justify uppercase">
+                                'OGUN, DO NOT FORSAKE ME'. OUR MAIN CHARACTER. HE IS THE EXPERIENCED WARRIOR WITH A
+                                CERTAIN TRAUMA WHICH HE SEEMS TO BE RUNNING AWAY FROM. HE IS KIND BUT RELUCTANT TO LET
+                                ANYONE GET TOO CLOSE TO HIM FOR FEAR THAT HE MIGHT LOSE THEM AND THAT HIS AFFECTION
+                                MIGHT GET IN THE WAY OF HIS ASSIGNMENT AT ANY GIVEN TIME.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Discord Server -->
+            <section>
+
+
+            </section>
+
+            <!-- Discord Server area -->
+            <section id="server" class="content-section latest-articles text-light"
+                     style="background-image: url('./src/assets/bg_shape.png')">
+                <div class="about-features content-section bp-c br-n bs-c">
+                    <div class="text-center container-fluid">
+
+                        <div class="row gutters-y">
+                            <div class="col-md-0 col-lg-2"></div>
+                            <div class="col-md-12 col-lg-8">
+                                <div class="display-4">
+                                    <!--                                    <widgetbot , channel="774034213228904457" class="mx-auto w-100"-->
+                                    <!--                                               height="600" server="774034212797808661">-->
+                                    <!--                                    </widgetbot>-->
+                                </div>
+                            </div>
+                            <div class="col-md-0 col-lg-2"></div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.Discord Server area -->
+
+            <!-- Socials -->
+            <section class="p-4">
+                <div class="">
+                    <h1 class="text-xl font-bold text-center">FOLLOW OUR PROGRESS</h1>
+                    <hr class="w-64 h-1.5 bg-amber-500 mx-auto">
+
+                    <div class="flex justify-evenly mt-4">
+                        <a href="" target="_blank">
+                            <img alt="" src="./assets/icons/icons8-instagram-48.png">
+                        </a>
+                        <a href="" target="_blank">
+                            <img alt="" src="./assets/icons/icons8-youtube-48.png">
+                        </a>
+                        <a href="" target="_blank">
+                            <img alt="" src="./assets/icons/icons8-discord-48.png">
+                        </a>
+                        <a href="" target="_blank">
+                            <img alt="" src="./assets/icons/icons8-linkedin-48.png">
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <footer class="p-4">
+                <img alt="" class="w-52" src="./assets/logo.png">
+                <h4 class="text-justify">
+                    LEGENDSOFORISHA: BLOOD AND WATER IS CURRENTLY UNDER DEVELOPMENT BY DIMENSION11 STUDIOS, A DIVERSE
+                    TEAM OF ARTISTS, PROGRAMMERS FROM ALL OVER THE WORLD, WITH IT'S HEADQUARTERS IN LAGOS, NIGERIA.
+                </h4>
+
+                <p class="mt-3 text-center">© DIMENSION 11 STUDIOS, {{ year }}. ALL RIGHTS RESERVED.</p>
+            </footer>
+        </div>
+    </main>
+
+</template>
+
+<script>
+    const date = new Date();
+
+    export default {
+        name: "Home",
+        data() {
+            return {
+                'year': date.getFullYear()
+            }
+        }
+    }
+</script>
