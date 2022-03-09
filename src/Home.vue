865c5f208e50561bef46dfd3df290c410838db69
Favour <toluwaniokunowo@gmail.com>
2022-03-09 11:37:05 +0100

    complete medium & large screen responsiveness

--- src/Home.vue
+++ src/Home.vue
@@ -4,7 +4,17 @@
 
             <header>
                 <img alt="Logo" class="mx-auto" src="./assets/logo.png" />
-                <img alt="Background Image" class="md:hidden" src="./assets/Mobile%20Portrait.jpg">
+                <img alt="Background Image"
+                     class="object-cover fixed top-0 left-0 w-screen h-screen opacity-30 md:hidden"
+                     src="./assets/Mobile%20Portrait.jpg">
+
+                <video autoplay class="hidden object-cover fixed top-0 left-0 w-screen h-screen opacity-30 md:block"
+                       loop muted
+                       playsinline poster="./assets/Mobile%20Portrait.jpg">
+                    <source src="./assets/promoVidSmall.mp4" type="video/mp4">
+                    <source src="./assets/promoVid3small.webm" type="video/webm">
+                    Your browser does not support this video.
+                </video>
             </header>
 
             <section class="px-6 py-4">
@@ -12,7 +22,7 @@
                     <h1 class="text-xl font-bold text-center">ABOUT</h1>
                     <hr class="w-20 h-1.5 bg-amber-500 mx-auto">
 
-                    <p class="mt-4 text-lg text-justify">
+                    <p class="mt-4 text-lg text-justify md:px-32">
                         A FORMER SOLDIER OF THE FALLEN ANCIENT EMPIRE OF OYO SEARCHES FOR REASON WHEN FATE AND HIS PAST
                         COME
                         HUNTING. REMILEKUN IS THRUST BACK INTO HIS ARMOUR WHEN HIS QUIET LIFE IN RETIREMENT IS
@@ -31,9 +41,9 @@
                 <h1 class="text-xl font-bold text-center">CHARACTERS</h1>
                 <hr class="w-36 h-1.5 bg-amber-500 mx-auto">
 
-                <div class="grid grid-cols-1 mt-4 md:grid-cols-2 lg:grid-cols-4">
+                <div class="grid grid-cols-1 gap-y-4 mt-4 md:grid-cols-2 md:gap-x-4">
                     <div>
-                        <img alt="" class="rounded-t-lg" src="./assets/cg_01.png">
+                        <img alt="" class="mx-auto rounded-t-lg" src="./assets/cg_01.png">
 
                         <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
                             <h3 class="text-2xl font-bold uppercase">Bamiloye</h3>
@@ -47,7 +57,7 @@
                     </div>
 
                     <div>
-                        <img alt="" class="rounded-t-lg" src="./assets/cg_02.png">
+                        <img alt="" class="mx-auto rounded-t-lg" src="./assets/cg_02.png">
 
                         <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
                             <h3 class="text-2xl font-bold uppercase">Iya Yemisi</h3>
@@ -61,7 +71,7 @@
                     </div>
 
                     <div>
-                        <img alt="" class="rounded-t-lg" src="./assets/cg_03.png">
+                        <img alt="" class="mx-auto rounded-t-lg" src="./assets/cg_03.png">
 
                         <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
                             <h3 class="text-2xl font-bold uppercase">Yemisi</h3>
@@ -77,7 +87,7 @@
                     </div>
 
                     <div>
-                        <img alt="" class="rounded-t-lg" src="./assets/cg_04.png">
+                        <img alt="" class="mx-auto rounded-t-lg" src="./assets/cg_04.png">
 
                         <div class="p-4 mt-3 space-y-4 rounded-b-lg outline outline-gray-600 outline-1">
                             <h3 class="text-2xl font-bold uppercase">Remilekun</h3>
@@ -123,36 +133,37 @@
             <!-- /.Discord Server area -->
 
             <!-- Socials -->
-            <section class="p-4">
-                <div class="">
-                    <h1 class="text-xl font-bold text-center">FOLLOW OUR PROGRESS</h1>
-                    <hr class="w-64 h-1.5 bg-amber-500 mx-auto">
+            <section class="px-6 py-4">
+                <h1 class="text-xl font-bold text-center">FOLLOW OUR PROGRESS</h1>
+                <hr class="w-64 h-1.5 bg-amber-500 mx-auto">
 
-                    <div class="flex justify-evenly mt-4">
-                        <a href="" target="_blank">
-                            <img alt="" src="./assets/icons/icons8-instagram-48.png">
-                        </a>
-                        <a href="" target="_blank">
-                            <img alt="" src="./assets/icons/icons8-youtube-48.png">
-                        </a>
-                        <a href="" target="_blank">
-                            <img alt="" src="./assets/icons/icons8-discord-48.png">
-                        </a>
-                        <a href="" target="_blank">
-                            <img alt="" src="./assets/icons/icons8-linkedin-48.png">
-                        </a>
-                    </div>
+                <div class="flex justify-evenly mt-4">
+                    <a href="" target="_blank">
+                        <img alt="" src="./assets/icons/icons8-instagram-48.png">
+                    </a>
+                    <a href="" target="_blank">
+                        <img alt="" src="./assets/icons/icons8-youtube-48.png">
+                    </a>
+                    <a href="" target="_blank">
+                        <img alt="" src="./assets/icons/icons8-discord-48.png">
+                    </a>
+                    <a href="" target="_blank">
+                        <img alt="" src="./assets/icons/icons8-linkedin-48.png">
+                    </a>
                 </div>
             </section>
 
-            <footer class="p-4">
-                <img alt="" class="w-52" src="./assets/logo.png">
-                <h4 class="text-justify">
-                    LEGENDSOFORISHA: BLOOD AND WATER IS CURRENTLY UNDER DEVELOPMENT BY DIMENSION11 STUDIOS, A DIVERSE
-                    TEAM OF ARTISTS, PROGRAMMERS FROM ALL OVER THE WORLD, WITH IT'S HEADQUARTERS IN LAGOS, NIGERIA.
-                </h4>
+            <footer class="px-6 py-4 border-t border-gray-600">
+                <div class="md:flex md:items-center md:gap-x-4">
+                    <img alt="" class="mx-auto w-52 md:mx-0" src="./assets/logo.png">
+                    <h4 class="col-span-2 text-sm text-justify">
+                        LEGENDSOFORISHA: BLOOD AND WATER IS CURRENTLY UNDER DEVELOPMENT BY DIMENSION11 STUDIOS, A
+                        DIVERSE TEAM OF ARTISTS, PROGRAMMERS FROM ALL OVER THE WORLD, WITH IT'S HEADQUARTERS IN LAGOS,
+                        NIGERIA.
+                    </h4>
+                </div>
 
-                <p class="mt-3 text-center">© DIMENSION 11 STUDIOS, {{ year }}. ALL RIGHTS RESERVED.</p>
+                <p class="mt-5 text-center">© DIMENSION 11 STUDIOS, {{ year }}. ALL RIGHTS RESERVED.</p>
             </footer>
         </div>
     </main>
